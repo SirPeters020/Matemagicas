175390c5f87780c032bdb5a5b4cf4df73ff21208
pedro augusto <pedro.augusto77@hotmail.com>
2021-06-10 17:55:59 -0300

    Feito Ate o Ex4

---
+++ Matemagicas.Ex3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Matemagicas.Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lendo valores
+            string anosTexto = Console.ReadLine();
+            // separando eles
+            string[] anos = anosTexto.Split(" ");
+            // colocando em variaveis separadas
+            int primeiroAno = Convert.ToInt32(anos[0]);
+            int segundoAno = Convert.ToInt32(anos[1]);
+
+            if (primeiroAno < segundoAno)
+            {
+                // contador de anos
+                int count = 0;
+                for (int i = primeiroAno; i <= segundoAno; i++)
+                {
+                    // se divisivel por 4
+                    if (i % 4 == 0)
+                    {
+                        // se for divisivel por 100 e não for por 400 vai dar vai dar verdadeiro
+                        // porem a "!" no inicio inverte, então dara false e não ira imprimir nas tela
+                        if (!(i % 100 == 0) && (i % 400 != 0))
+                        {
+                            Console.WriteLine(i);
+                            count++;
+                        }
+                    }
+                }
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine("Erro: O primeiro ano deve ser menor que o segundo.");
+            }
+
+            
+        }
+    }
+}
---
+++ Matemagicas.Ex4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Matemagicas.Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escolha uma opção, Tempo(1), Valor futuro (2), Investimento necessario(3), Sair(4)");
+            int opcao = Convert.ToInt32(Console.ReadLine());
+
+            // declarando todas as variaveis que vou usar
+            double investimento = 0;
+            double montante = 0;
+            double juros = 0;
+            double valorAtualizado = 0;
+            int contadorMeses = 0;
+            int tempo = 0;
+            switch (opcao)
+            {
+                case 1:
+                    investimento = Convert.ToDouble(Console.ReadLine());
+                    montante = Convert.ToDouble(Console.ReadLine());
+                    juros = Convert.ToDouble(Console.ReadLine());
+                    valorAtualizado = investimento;
+                    contadorMeses = 0;
+
+                    while (valorAtualizado < montante)
+                    {
+                        var valorAdicional = (((juros * 100) * valorAtualizado) / 10000); ;
+                        valorAtualizado += valorAdicional;
+                        contadorMeses++;
+                    }
+                    Console.WriteLine(contadorMeses);
+                    break;
+                case 2:
+                    investimento = Convert.ToDouble(Console.ReadLine());
+                    juros = Convert.ToDouble(Console.ReadLine());
+                    tempo = Convert.ToInt32(Console.ReadLine());
+                    valorAtualizado = investimento;
+                    for (int i = 0; i <= tempo; i++)
+                    {
+                        var valorAdicional = (((juros * 100) * valorAtualizado) / 10000);
+                        valorAtualizado += valorAdicional;
+                    }
+                    Console.WriteLine(valorAtualizado);
+                    break;
+                case 3:
+                    montante = Convert.ToDouble(Console.ReadLine());
+                    juros = Convert.ToDouble(Console.ReadLine());
+                    tempo = Convert.ToInt32(Console.ReadLine());
+                    valorAtualizado = montante;
+                    for (int i = 0; i <= tempo; i++)
+                    {
+                        valorAtualizado -= (((juros * 100) * valorAtualizado) / 10000);
+                    }
+                    Console.WriteLine(valorAtualizado);
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida !");
+                    break;
+            }
+
+        }
+    }
+}
